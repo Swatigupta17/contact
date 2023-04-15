1027d81f8dbe07c2c90f7de9ef8c0e840d0be344
Swatigupta17 <106000591+Swatigupta17@users.noreply.github.com>
2023-04-15 22:11:17 +0530

    Add files via upload

---
+++ doctors.js
@@ -0,0 +1,10 @@
+
+function openpopup(){
+    let popup=document.getElementById("popup");
+    popup.classList.add("open-popup");
+}
+
+function closepopup(){
+    let popup=document.getElementById("popup");
+    popup.classList.remove("open-popup");
+}
